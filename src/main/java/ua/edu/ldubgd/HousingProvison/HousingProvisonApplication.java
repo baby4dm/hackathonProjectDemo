93457f28a6fba35fea63a31167f10ecde5060642
8dimas8 <dimikoff008@gmail.com>
2023-11-27 10:28:23 +0200

    starting commit

---
+++ src/main/java/ua/edu/ldubgd/HousingProvison/HousingProvisonApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.ldubgd.HousingProvison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingProvisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousingProvisonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/edu/ldubgd/HousingProvison/HousingProvisonApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.ldubgd.HousingProvison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousingProvisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
